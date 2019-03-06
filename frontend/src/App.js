b161fcc352f26b4152942073e22cc5d28fdb9e87
Cezary Miącz <cezary.miacz@stxnext.pl>
2019-03-06 14:46:27 +0100

    Added draftjs editor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import Editor, { createEditorStateWithText } from 'draft-js-plugins-editor';
+import createToolbarPlugin from 'draft-js-static-toolbar-plugin';
+
+import 'draft-js-static-toolbar-plugin/lib/plugin.css';
 import './App.css';
 
+
+const staticToolbarPlugin = createToolbarPlugin();
+const { Toolbar } = staticToolbarPlugin;
+
+const text = 'The toolbar below the editor can be used for formatting text, as in conventional static editors.';
+
 class App extends Component {
+
+  state = {
+    editorState: createEditorStateWithText(text),
+  };
+
+  focus = () => this.editor.focus();  
+
+  setEditorRef = element => this.editor = element;
+
+  onChange = editorState => this.setState({ editorState });
+
   render() {
     return (
-      <div className="App">
-        Test
+      <div class="flex-container">
+        <h3>Text Editor</h3>
+        <div className="editor">
+          <Editor
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+            plugins={[staticToolbarPlugin]}
+            ref={this.setEditorRef}
+          />
+          <Toolbar />
+        </div>
+        <button>
+          Export to docx
+        </button>
       </div>
     );
   }
